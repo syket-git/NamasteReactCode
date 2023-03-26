a7805f48c33d0492923e83b8566e9457930bf005
syketbb <syket@algosolver.com>
2023-03-26 23:49:01 +0600

    jsx implement

--- class3/App.js
+++ class3/App.js
@@ -1,19 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-const spDiv = React.createElement(
-  'div',
-  { className: 'super_parent' },
-  React.createElement(
-    'div',
-    { className: 'immediate_parent' },
-    React.createElement(
-      'h1',
-      { className: 'title' },
-      React.createElement('p', {}, 'Syket Bhattachergee')
-    )
-  )
-);
+const Title = () => {
+  return <h1>Syket Bhattachergee</h1>;
+};
 
+const Header = () => (
+  <div>
+    {<Title />}
+    <h1>Header</h1>
+  </div>
+);
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(spDiv);
+root.render(<Header />);
